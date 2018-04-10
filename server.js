d93716b5f885f324d055712995ebad44dab0d58e
Otto <o.stegmaier@gmail.com>
2018-04-09 19:47:24 -0700

    Handle env variables on GAE.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+'use strict';
+
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/api/*', (req, res) => {
-  res.json({ok: true});
+  res.json({ ok: true });
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
