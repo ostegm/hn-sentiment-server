59fd0197480eeffb406ff0d42a37b700c9c98590
Otto <o.stegmaier@gmail.com>
2018-04-10 21:24:38 -0700

    Add address for HN Api

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/my-library';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-my-library';
 exports.PORT = process.env.PORT || 8080;
+exports.HACKER_NEWS_API = 'https://hacker-news.firebaseio.com/v0/item/';
