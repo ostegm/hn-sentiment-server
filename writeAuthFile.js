51d0f727a5176c6b9290a2862cafccd3ba3bc200
Otto <o.stegmaier@gmail.com>
2018-07-10 23:58:56 -0700

    Comments

--- writeAuthFile.js
+++ writeAuthFile.js
@@ -5,5 +5,5 @@ const keysEnvVar = process.env.GAE_CREDS;
 if (!keysEnvVar) {
   throw new Error('The $CREDS environment variable was not found!');
 }
-const keys = JSON.parse(keysEnvVar);
+// Write to a json file to be used by the Google Auth Library.
 fs.writeFile('hn-sentiment-3b3b291ae079.json', keysEnvVar, 'utf8');
